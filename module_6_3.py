281e9e1fd12747a819fd49d08760927dba179a93
skynavi <skynavi@mail.ru>
2024-08-16 00:53:34 +0300

    1608

--- module_6_3.py
+++ module_6_3.py
@@ -5,34 +5,33 @@ class Horse:
 
     x_distance = 0  # - пройденный путь.
     sound = 'Frrr'
+
     def run(self, dx):
         self.x_distance += dx
         self.x_distance = self.x_distance
 
 
-class Eagle:  #  класс описывающий орла. Объект этого класса обладает следующими атрибутами
+class Eagle:  # класс описывающий орла. Объект этого класса обладает следующими атрибутами
 
     def __init__(self):
         print('init eagle')
-    y_distance = 0   #  - высота полёта
+
+    y_distance = 0  # - высота полёта
 
     def fly(self, dy):
         self.y_distance += dy
         self.y_distance = self.y_distance
 
-
-    sound = 'I train, eat, sleep, and repeat'   # - звук, который издаёт орёл (отсылка)
-
+    sound = 'I train, eat, sleep, and repeat'  # - звук, который издаёт орёл (отсылка)
 
 
-class Pegasus(Horse, Eagle):    #  - класс описывающий пегаса. Наследуется от Horse и Eagle
+class Pegasus(Horse, Eagle):  # - класс описывающий пегаса. Наследуется от Horse и Eagle
     def move(self, dx, dy):
         self.dx = super().run(dx)
         self.dy = super().fly(dy)
 
-
     def get_pos(self):
-        return self.x_distance, self.y_distance # возвращает текущее положение пегаса в виде кортежа
+        return self.x_distance, self.y_distance  # возвращает текущее положение пегаса в виде кортежа
 
     def voice(self):
         print(Eagle.sound)
